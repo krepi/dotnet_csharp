7e1cf79cb14a8f67a8d7b08a842e0b9df0b21dd7
przemyslawkrepski <krepski80@gmail.com>
2024-01-01 15:14:54 +0100

    after first app

--- Program.cs
+++ Program.cs
@@ -6,23 +6,12 @@ using System.Text;
 
 namespace FirstProject
 {
+    
     internal class Program
     {
-        static Dictionary<string, List<int>> grades = new Dictionary<string, List<int>>();     
         private static void Main(string[] args)
         {
-grades.Add("bob",new List<int>(){5,6});
-
-{
-    
-} 
-            grades["bob"].Add(5);
-            Console.WriteLine(grades["bob"].Count);
-            foreach(int grade in grades["bob"] )
-            {
-                
-            }
-
+            
         }
     }
 }
